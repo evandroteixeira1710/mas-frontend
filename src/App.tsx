9b0febfe685c5fc9548585ba7200dbe3cf76a200
Evandro Teixeira <evandro_teixeira@hotmail.com>
2021-11-14 22:16:42 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,3 @@ function App() {
 }
 
 export default App;
-
-/*import {BrowserRouter as Router} from 'react-router-dom';
-import Routes from './routes';
-import  GlobalStyle from './styles/global';
-
-
-function App() {
-  return (
-    <Router>
-      <Routes />
-      <GlobalStyle />
-    </Router>
-  );
-}
-
-export default App;
-*/
