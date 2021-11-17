598662e26a9437a8ad0afdfa866c459254e100de
Evandro Teixeira <evandro_teixeira@hotmail.com>
2021-11-17 20:57:41 -0300

    Alteraçao Heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'production' 
+    ?'https://mas-backend-senaii.herokuapp.com'
+    :'http://localhost:3333',
+});
+
+export default api;
+
+/*
+import axios from 'axios';
+
+const api = axios.create({
     baseURL: "http://localhost:3334",
    });
    export default api;
+*/   
